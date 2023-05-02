e1938b97706285793538e90fd3b8d200730e70e8
jahmed93 <dev.jahmed@gmail.com>
2023-05-02 16:33:08 -0700

    frontend edits TODO stx

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -9,8 +9,9 @@ import {
 } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 //style
-import { shades } from "../../theme";
-import classes from '../NavBar.module.css';
+import { shades } from "./../theme";
+import classes from './NavBar.module.css';
+import { setIsCartOpen } from './../state';
 
 
 
@@ -18,7 +19,10 @@ import classes from '../NavBar.module.css';
 function NavBar (){
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.cart);
 
+
+    
     return (
         <Box className={classes.outterBox}>
             <Box className={classes.innerBox}>
@@ -30,9 +34,23 @@ function NavBar (){
                     <IconButton classname={classes.iconButton}>
                         <PersonOutline />
                     </IconButton>
-                    <IconButton classname={classes.iconButton}>
+                    <Badge
+                        badgeContent={cart.length}
+                        color="secondary"
+                        invisible={cart.legth === 0}
+                        sx={{"& .MuiBadge-badge": {
+                            right: 5,
+                            top: 5,
+                            padding: "0 4px",
+                            height: "14px",
+                            minWidth: "13px",
+                        },
+                    }}
+                        >
+                    <IconButton classname={classes.iconButton} onClick={() => dispatch(setIsCartOpen({}))}>
                         <ShoppingBagOutlined />
                     </IconButton>
+                    </Badge>
                     <IconButton classname={classes.iconButton}>
                         <MenuOutlined />
                     </IconButton>
